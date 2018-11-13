b2bbfdad7078f2835d85cf451bff798a75e0b10e
f8B23Lef <mal7rish@yandex.ru>
2018-11-13 16:58:25 +0300

    feat: modify http-request function for searching user input

--- youtube-client/js/script.js
+++ youtube-client/js/script.js
@@ -1,38 +1,65 @@
-function makeRequest(searchStr = "Funny cats") {
-  var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=cat&key=AIzaSyCsfImNexVdmlnxQKZHsELAd4_45JuTbQQ";
+function makeVideoIdsRequest() {
+  var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCsfImNexVdmlnxQKZHsELAd4_45JuTbQQ&q=${getUserInput()}`;
+  console.log('url = ', url);
   fetch(url).then(function(response) {
     if (response.ok) {
+      const videoIds = [];
       response.json()
-      .then(function(result) {
-        result.items.forEach(item => {
-          console.log(item);
-          parseResult(item);
+        .then(function(result) {
+          result.items.forEach(item => {
+            videoIds.push(getVideoId(item));
+          });
+        })
+        .then(function() {
+          makeDataRequest(formVideoIdsStr(videoIds));
         });
-      });
     } else {
       console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
     }
   }, function(error) {
     console.log(error);
   });
-  
-    // var request = new XMLHttpRequest();
-    // request.open('GET', url, true);
-    // request.onreadystatechange = function() {
-    //   // if (this.readyState == 4 && this.status == 200) {
-    //     console.log(this.readyState);
-    //     console.log(this.status);
-    //     console.log(request.responseText);
-    //     console.log(this);
-    //   // }
-    // };
-    // request.send();
-  };
+};
+
+function makeDataRequest(videoIdsStr) {
+  var url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&key=AIzaSyCsfImNexVdmlnxQKZHsELAd4_45JuTbQQ&id=${videoIdsStr}`;
+  fetch(url).then(function(response) {
+    if (response.ok) {
+      response.json()
+        .then(function(result) {
+          result.items.forEach(item => {
+            // console.log(item);
+            parseResult(item);
+          });
+        });
+    } else {
+      console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
+    }
+  }, function(error) {
+    console.log(error);
+  });
+};
+
+const getVideoId = function (item) {
+  const id = item.id.videoId;
+  return id;
+}
+
+const formVideoIdsStr = function (videoId) {
+  const videoIdsStr = videoId.join(',');
+  return videoIdsStr;
+}
+
+const getUserInput = function () {
+  const searchStr = document.querySelector('.search-form__input').value;
+  console.log('searchStr = ', searchStr);
+  return searchStr;
+}
 
 const parseResult = function (result) {
   const title = result.snippet.title;
   console.log('title: ', title);
-  const link = `https://www.youtube.com/watch?v=${result.id.videoId}`;
+  const link = `https://www.youtube.com/watch?v=${result.id}`;
   console.log('link: ', link);
   const author = result.snippet.channelTitle;
   console.log('author: ', author);
@@ -43,17 +70,18 @@ const parseResult = function (result) {
   console.log('description: ', description);
   const imageUrl = result.snippet.thumbnails.medium.url;
   console.log('imageURL: ', imageUrl);
-  console.log('view: ');
+  const viewCount = result.statistics.viewCount;
+  console.log('viewCount: ', viewCount);
 };
 
 const onKeyDown = function (key) {
   if (key.code == 'Enter') {
     key.preventDefault();
-    makeRequest();
+    makeVideoIdsRequest();
   }
 };
 
 window.onload = function () {
-  document.querySelector('.search-form__button').addEventListener('click', makeRequest);
+  document.querySelector('.search-form__button').addEventListener('click', makeVideoIdsRequest);
   document.addEventListener('keydown', onKeyDown);
 };
