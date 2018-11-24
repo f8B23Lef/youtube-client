3437fc14cf51c088c74638e86691e22ba5eb9d5a
f8B23Lef <mal7rish@yandex.ru>
2018-11-24 22:57:57 +0300

    refactor: delete comments and add commas

--- youtube-client/webpack.config.js
+++ youtube-client/webpack.config.js
@@ -4,17 +4,17 @@ const conf = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: '/node_modules/'
-      }
-    ]
-  }
+        exclude: '/node_modules/',
+      },
+    ],
+  },
 };
 
 module.exports = conf;
